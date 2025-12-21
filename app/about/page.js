bd2fee5f3a5861e91c95f178e883a388cd6a507b
Mamta-Developer <mamta55developer@gmail.com>
2025-12-22 05:26:58 +0530

    New

--- app/about/page.js
+++ app/about/page.js
@@ -2,9 +2,9 @@ export default function AboutPage() {
   return (
     <section
       className="min-h-screen flex items-center justify-center bg-[url('/images/space.jpg')] bg-cover bg-top
-        overflow-hidden"
+        overflow-hidden  px-6 md:px-16"
     >
-      <h1 className="text-4xl font-bold">Coming Soon (About Page)</h1>
+      <h1 className="text-lg md:text-4xl font-bold text-center">Coming Soon (About Page)</h1>
     </section>
   );
 }
--- app/events/page.js
+++ app/events/page.js
@@ -2,9 +2,9 @@ export default function EventsPage() {
   return (
     <section
       className="min-h-screen flex items-center justify-center bg-[url('/images/space.jpg')] bg-cover bg-top
-        overflow-hidden"
+        overflow-hidden px-6 md:px-16"
     >
-      <h1 className="text-4xl font-bold">Coming Soon (Events Page)</h1>
+      <h1 className="text-lg md:text-4xl font-bold text-center">Coming Soon (Events Page)</h1>
     </section>
   );
 }
