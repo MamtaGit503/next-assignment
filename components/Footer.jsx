9ca23779b35aefa2eb49bf16ac6284e17b39e10a
Mamta <131393051+MamtaGit503@users.noreply.github.com>
2025-12-21 17:58:10 +0530

    Refactor footer layout and simplify menu structure

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,34 +4,33 @@ export default function Footer() {
       <div className="absolute inset-0 "></div>
 
       <div className="relative py-6">
-        <div className="flex flex-col md:flex-row md:items-center justify-between gap-6 text-sm">
+        <ul className="flex flex-col md:flex-row flex-wrap justify-center gap-4 md:gap-8 tracking-wide text-[16px] md:text-[18px]">
+          <li className="text-white cursor-pointer hover:text-gray-400">
+            Home
+          </li>
+          <li className="text-white cursor-pointer hover:text-gray-400">
+            Experts
+          </li>
+          <li className="text-white cursor-pointer hover:text-gray-400">
+            About us
+          </li>
+          <li className="text-white cursor-pointer hover:text-gray-400">
+            Podcasts
+          </li>
+          <li className="text-white cursor-pointer hover:text-gray-400">
+            Contacts
+          </li>
+        </ul>
+
+        <div className="flex flex-col md:flex-row md:items-center justify-between gap-6 text-sm mt-5">
           {/* Left */}
-          <div className="flex items-center gap-2 cursor-pointer hover:text-white transition md:mt-20 text-[16px] md:text-[18px]">
+          <div className="flex items-center gap-2 cursor-pointer hover:text-white transition  text-[16px] md:text-[18px]">
             <span className="text-lg">💬</span>
             <span>Let’s chat</span>
           </div>
 
-          {/* Center Menu */}
-          <ul className="flex md:flex-row flex-col md:justify-center gap-3 md:gap-8 tracking-wide text-[13px] md:text-[18px]">
-            <li className="text-white cursor-pointer hover:text-gray-400">
-              Home
-            </li>
-            <li className="text-white cursor-pointer hover:text-gray-400">
-              Experts
-            </li>
-            <li className="text-white cursor-pointer hover:text-gray-400">
-              About us
-            </li>
-            <li className="text-white cursor-pointer hover:text-gray-400">
-              Podcasts
-            </li>
-            <li className="text-white cursor-pointer hover:text-gray-400">
-              Contacts
-            </li>
-          </ul>
-
           {/* Right */}
-          <div className="flex items-center  gap-2 text-[16px] md:text-[18px] hover:text-white transition cursor-pointer md:mt-20">
+          <div className="flex items-center gap-2 text-[16px] md:text-[18px] hover:text-white transition cursor-pointer ">
             <span>✉</span>
             <span>info@logoipsum.com</span>
           </div>
